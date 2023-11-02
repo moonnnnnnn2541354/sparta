d3f0cbb886968a18cda4b7f7d7f462ac13d40f59
tast <ioae7979@gmail.com>
2023-11-02 12:21:07 +0900

    Spring 프로젝트 생성

---
+++ 4. Spring 입문주차 KDT실무형/sparta-prepare/src/main/java/com/example/spartaprepare/SpartaPrepareApplication.java
@@ -0,0 +1,13 @@
+package com.example.spartaprepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaPrepareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaPrepareApplication.class, args);
+	}
+
+}
---
+++ 4. Spring 입문주차 KDT실무형/sparta-prepare/src/test/java/com/example/spartaprepare/SpartaPrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spartaprepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaPrepareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
