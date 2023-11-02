b20e1fd6fcdb497220285d907a9826436f23a6bc
tast <ioae7979@gmail.com>
2023-11-02 18:00:47 +0900

    controller 심화

---
+++ 4. Spring 입문주차 KDT실무형/spring-mvc/src/main/java/com/sparta/springmvc/controller/HelloController.java
@@ -0,0 +1,38 @@
+package com.sparta.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api") // 각 Mapping들 주소값에 "/api"를 넣음
+public class HelloController {
+    @GetMapping("/hello") // /api/hello
+    @ResponseBody
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/get") // /api/get
+    @ResponseBody
+    public String get() {
+        return "GET Method 요청";
+    }
+
+    @PostMapping("/post") // /api/post
+    @ResponseBody
+    public String post() {
+        return "POST Method 요청";
+    }
+
+    @PutMapping("/put") // /api/put
+    @ResponseBody
+    public String put() {
+        return "PUT Method 요청";
+    }
+
+    @DeleteMapping("/delet") // /api/delet
+    @ResponseBody
+    public String delet() {
+        return "DELETE Method 요청";
+    }
+}
