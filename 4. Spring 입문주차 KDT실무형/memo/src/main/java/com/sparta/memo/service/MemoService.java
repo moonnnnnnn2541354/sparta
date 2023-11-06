40d796121609f1fee763fd7fa87b7670b56f8453
tast <ioae7979@gmail.com>
2023-11-06 13:02:09 +0900

    ioC container와 Bean

--- 4. Spring 입문주차 KDT실무형/memo/src/main/java/com/sparta/memo/service/MemoService.java
+++ 4. Spring 입문주차 KDT실무형/memo/src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,25 +4,29 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
+//@Component
+@Service
 public class MemoService {
-
 
     private final MemoRepository memoRepository;
 
     public MemoService(MemoRepository memoRepository) {
         this.memoRepository = memoRepository;
     }
+
+//    public MemoService (ApplicationContext context) {
+//        // 1. 'Bean' 이름으로 가져오기
+//        MemoRepository memoRepository = (MemoRepository) context.getBean("memoRepository");
+//        // 2. 'Bean' 클래스 형식으로 가져오기
+//        MemoRepository memoRepository = context.getBean(MemoRepository.class);
+//        this.memoRepository = memoRepository;
+//    }
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
         // RequestDto -> Entity
         Memo memo = new Memo(requestDto);
