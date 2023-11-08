9787bf61420044fe889cc1987d923f900a06cb48
tast <ioae7979@gmail.com>
2023-11-08 16:05:45 +0900

    SpringBoot의 JPA

--- 4. Spring 입문주차 KDT실무형/memo/src/main/java/com/sparta/memo/entity/Memo.java
+++ 4. Spring 입문주차 KDT실무형/memo/src/main/java/com/sparta/memo/entity/Memo.java
@@ -1,17 +1,25 @@
 package com.sparta.memo.entity;
 
 import com.sparta.memo.dto.MemoRequestDto;
+import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Getter
 @Setter
+@Table(name = "memo") // 매핑할 테이블의 이름을 지정
 @NoArgsConstructor
 public class Memo {
-    private Long id; // id
-    private String username; // 작성자
-    private String contents; // 내용
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "contents", nullable = false, length = 500)
+    private String contents;
 
     public Memo(MemoRequestDto requestDto) {
         this.username = requestDto.getUsername();
