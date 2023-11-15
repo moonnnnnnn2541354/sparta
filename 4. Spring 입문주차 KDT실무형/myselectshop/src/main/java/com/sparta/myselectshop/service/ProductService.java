9d6a1ff08910032df773f3e29f4348fabec0969d
tast <ioae7979@gmail.com>
2023-11-15 19:39:50 +0900

    Scheduler 기능 구현

--- 4. Spring 입문주차 KDT실무형/myselectshop/src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ 4. Spring 입문주차 KDT실무형/myselectshop/src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,6 +4,7 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import jakarta.validation.constraints.Null;
 import java.util.ArrayList;
@@ -49,5 +50,12 @@ public class ProductService {
             responseDtoList.add(new ProductResponseDto(product));
         }
         return responseDtoList;
+    }
+
+    @Transactional
+    public void updateBySearch(Long id, ItemDto itemDto) {
+        Product product = productRepository.findById(id).orElseThrow(
+            () -> new NullPointerException("해당 상품은 찾을 수 없습니다."));
+        product.updateByItemDto(itemDto);
     }
 }
