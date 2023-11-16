95d86ea3a507dfe60e00be2d7e1c1c51f139900c
tast <ioae7979@gmail.com>
2023-11-16 12:04:51 +0900

    폴더 생성 및 조회

--- 4. Spring 입문주차 KDT실무형/myselectshop/src/main/java/com/sparta/myselectshop/repository/FolderRepository.java
+++ 4. Spring 입문주차 KDT실무형/myselectshop/src/main/java/com/sparta/myselectshop/repository/FolderRepository.java
@@ -1,8 +1,14 @@
 package com.sparta.myselectshop.repository;
 
 import com.sparta.myselectshop.entity.Folder;
-import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import com.sparta.myselectshop.entity.User;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FolderRepository extends JpaAttributeConverter<Folder,Long> {
+public interface FolderRepository extends JpaRepository<Folder,Long> {
 
+    // 예시) select * from folder where user_id = 1 and name in ('1','2','3');
+    List<Folder> findAllByUserAndNameIn(User user, List<String> folderNames);
+
+    List<Folder> findAllByUser(User user);
 }
