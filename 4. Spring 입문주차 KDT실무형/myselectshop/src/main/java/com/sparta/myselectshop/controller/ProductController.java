3329b2693431417e63848849e16c00969f65f047
tast <ioae7979@gmail.com>
2023-11-29 16:44:16 +0900

    Spring AOP 적용

--- 4. Spring 입문주차 KDT실무형/myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ 4. Spring 입문주차 KDT실무형/myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -3,12 +3,8 @@ package com.sparta.myselectshop.controller;
 import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
-import com.sparta.myselectshop.entity.ApiUseTime;
-import com.sparta.myselectshop.entity.User;
-import com.sparta.myselectshop.repository.ApiUseTimeRepository;
 import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.ProductService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -27,72 +23,45 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
-    private final ApiUseTimeRepository apiUseTimeRepository;
 
     @PostMapping("/products")
-    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // 측정 시작 시간
-        long startTime = System.currentTimeMillis();
-
-        try {
-            // 응답 보내기
-            return productService.createProduct(requestDto, userDetails.getUser());
-        } finally {
-            // 측정 종료 시간
-            long endTime = System.currentTimeMillis();
-            // 수행시간 = 종료 시간 - 시작 시간
-            long runTime = endTime - startTime;
-
-            // 로그인 회원 정보
-            User loginUser = userDetails.getUser();
-
-            // API 사용시간 및 DB 에 기록
-            ApiUseTime apiUseTime = apiUseTimeRepository.findByUser(loginUser)
-                .orElse(null);
-            if (apiUseTime == null) {
-                // 로그인 회원의 기록이 없으면
-                apiUseTime = new ApiUseTime(loginUser, runTime);
-            } else {
-                // 로그인 회원의 기록이 이미 있으면
-                apiUseTime.addUseTime(runTime);
-            }
-
-            System.out.println("[API Use Time] Username: " + loginUser.getUsername() + ", Total Time: " + apiUseTime.getTotalTime() + " ms");
-            apiUseTimeRepository.save(apiUseTime);
-        }
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return productService.createProduct(requestDto, userDetails.getUser());
     }
 
     @PutMapping("/products/{id}")
-    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto){
-        return productService.updateProduct(id,requestDto);
+    public ProductResponseDto updateProduct(@PathVariable Long id,
+        @RequestBody ProductMypriceRequestDto requestDto) {
+        return productService.updateProduct(id, requestDto);
     }
 
     @GetMapping("/products")
     public Page<ProductResponseDto> getProducts(
-            @RequestParam("page") int page,
-            @RequestParam("size") int size,
-            @RequestParam("sortBy") String sortBy,
-            @RequestParam("isAsc") boolean isAsc,
-            @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return productService.getProducts(userDetails.getUser(),page-1,size,sortBy,isAsc);
+        @RequestParam("page") int page,
+        @RequestParam("size") int size,
+        @RequestParam("sortBy") String sortBy,
+        @RequestParam("isAsc") boolean isAsc,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return productService.getProducts(userDetails.getUser(), page - 1, size, sortBy, isAsc);
     }
 
     @PostMapping("/products/{productId}/folder")
     public void addFolder(@PathVariable Long productId,
-                            @RequestParam Long folderId,
-                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        productService.addFolder(productId,folderId,userDetails.getUser());
+        @RequestParam Long folderId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        productService.addFolder(productId, folderId, userDetails.getUser());
     }
 
     @GetMapping("/folders/{folderId}/products")
-    public Page<ProductResponseDto> getProdutsInFolder (
-            @PathVariable Long folderId,
-            @RequestParam("page") int page,
-            @RequestParam("size") int size,
-            @RequestParam("sortBy") String sortBy,
-            @RequestParam("isAsc") boolean isAsc,
-            @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return productService.getProductsInFolder (
-            folderId, page-1, size, sortBy, isAsc, userDetails.getUser());
+    public Page<ProductResponseDto> getProdutsInFolder(
+        @PathVariable Long folderId,
+        @RequestParam("page") int page,
+        @RequestParam("size") int size,
+        @RequestParam("sortBy") String sortBy,
+        @RequestParam("isAsc") boolean isAsc,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return productService.getProductsInFolder(
+            folderId, page - 1, size, sortBy, isAsc, userDetails.getUser());
     }
 }
